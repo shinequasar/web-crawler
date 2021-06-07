bbd7fcd0225811109f558b5ff5155d7d921cf0c0
shinequasar <shinequasar@gmail.com>
2021-06-07 16:48:18 +0900

    feat:페이지별 크롤링, 긁은 값들 CSV 파일로 저장

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.contestkorea.com/sub/list.php?int_gbn=1&Txt_bcode=030210001"
+f = open("event_list.csv","w", encoding="utf-8-sig", newline="")
+writer = csv.DictWriter(f,
+                        fieldnames=["제목", "주최", "대상", "날짜"])
+writer.writeheader()
 
-data = requests.get(url)
+for page_num in range(1, 33):
+    url = f"https://www.contestkorea.com/sub/list.php?displayrow=12&int_gbn=1&Txt_sGn=1&Txt_key=all&Txt_word=&Txt_bcode=030210001&Txt_code1=&Txt_aarea=&Txt_area=&Txt_sortkey=a.int_sort&Txt_sortword=desc&Txt_host=&Txt_tipyn=&Txt_actcode=&page={page_num}"
 
-if data.status_code != requests.codes.ok:
-    print("접속실패")
-    exit()
+    data = requests.get(url)
 
-html = BeautifulSoup(data.text, "html.parser")
+    if data.status_code != requests.codes.ok:
+        print("접속실패")
+        exit()
 
-events = html.select('.list_style_2 ul > li')
-for event in events[::3]:
-    title = event.select_one('.title .txt').text
-    host = event.select_one('.host li').text.strip().replace("주최","").replace(".","")
-    target = event.select_one('li .icon_2').text.strip().replace("대상","").replace(".","").replace(" ","")
-    date = event.select_one('.date div').text
-    print(title, host, target, date)
+    html = BeautifulSoup(data.text, "html.parser")
+
+    events = html.select('.list_style_2 ul > li')
+    for event in events[::3]:
+        data_dict = {}
+        title = event.select_one('.title .txt').text
+        host = event.select_one('.host li').text.strip().replace("주최","").replace(".","")
+        target = event.select_one('li .icon_2').text.strip().replace("대상","").replace(".","").replace(" ","")
+        date = event.select_one('.date div').text
+
+        data_dict["제목"] = title
+        data_dict["주최"] = host
+        data_dict["대상"] = target
+        data_dict["날짜"] = date
+        writer.writerow(data_dict)
+        print(title, host, target, date)
+
+f.close()
